2c8ac52680ddc5c67b68f28dc56f47d91f7a2768
Grovre <50428844+Grovre@users.noreply.github.com>
2022-04-23 14:13:27 -0500

    Added color to Tracked.java

--- src/main/java/github/grovre/playerstatistics/statistics/Tracked.java
+++ src/main/java/github/grovre/playerstatistics/statistics/Tracked.java
@@ -1,17 +1,21 @@
 package github.grovre.playerstatistics.statistics;
 
+import org.bukkit.ChatColor;
+
 public enum Tracked {
-    BROKEN_BLOCKS,
-    PLACED_BLOCKS,
-    KILLED_MOBS,
-    SERVER_JOINS,
-    BEDTIME,
-    KICKS,
-    COMMANDS_SENT,
-    DROPPED_ITEMS,
-    EXP_GAINED,
-    THROWN_EGGS,
-    CAUGHT_FISH,
-    MESSAGES_SENT,
-    CHARACTERS_TYPED
+    BROKEN_BLOCKS (ChatColor.DARK_BLUE),
+    PLACED_BLOCKS (ChatColor.BLUE),
+    KILLED_MOBS (ChatColor.RED),
+    SERVER_JOINS (ChatColor.GREEN),
+    BEDTIME (ChatColor.RED),
+    KICKS (ChatColor.RED),
+    COMMANDS_SENT (ChatColor.LIGHT_PURPLE),
+    DROPPED_ITEMS (ChatColor.LIGHT_PURPLE),
+    EXP_GAINED (ChatColor.GREEN),
+    THROWN_EGGS (ChatColor.YELLOW),
+    CAUGHT_FISH (ChatColor.YELLOW),
+    MESSAGES_SENT (ChatColor.AQUA),
+    CHARACTERS_TYPED (ChatColor.DARK_AQUA);
+
+    Tracked(ChatColor color) {}
 }
