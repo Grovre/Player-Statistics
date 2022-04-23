1876720fe29f0da949b76cb88312e9fbbaf6982b
Grovre <50428844+Grovre@users.noreply.github.com>
2022-04-23 15:07:19 -0500

    trackers now have names

--- src/main/java/github/grovre/playerstatistics/statistics/Tracked.java
+++ src/main/java/github/grovre/playerstatistics/statistics/Tracked.java
@@ -3,19 +3,24 @@ package github.grovre.playerstatistics.statistics;
 import org.bukkit.ChatColor;
 
 public enum Tracked {
-    BROKEN_BLOCKS (ChatColor.DARK_BLUE),
-    PLACED_BLOCKS (ChatColor.BLUE),
-    KILLED_MOBS (ChatColor.RED),
-    SERVER_JOINS (ChatColor.GREEN),
-    BEDTIME (ChatColor.RED),
-    KICKS (ChatColor.RED),
-    COMMANDS_SENT (ChatColor.LIGHT_PURPLE),
-    DROPPED_ITEMS (ChatColor.LIGHT_PURPLE),
-    EXP_GAINED (ChatColor.GREEN),
-    THROWN_EGGS (ChatColor.YELLOW),
-    CAUGHT_FISH (ChatColor.YELLOW),
-    MESSAGES_SENT (ChatColor.AQUA),
-    CHARACTERS_TYPED (ChatColor.DARK_AQUA);
+    BROKEN_BLOCKS (ChatColor.DARK_BLUE, "Broken Blocks"),
+    PLACED_BLOCKS (ChatColor.BLUE, "Placed Blocks"),
+    KILLED_MOBS (ChatColor.RED, "Killed Mobs"),
+    SERVER_JOINS (ChatColor.GREEN, "Server Joins"),
+    BEDTIME (ChatColor.RED, "Bedtimes"),
+    KICKS (ChatColor.RED, "Server Kicks"),
+    COMMANDS_SENT (ChatColor.LIGHT_PURPLE, "Sent Commands"),
+    DROPPED_ITEMS (ChatColor.LIGHT_PURPLE, "Items Dropped"),
+    EXP_GAINED (ChatColor.GREEN, "Experience Gained"),
+    THROWN_EGGS (ChatColor.YELLOW, "Eggs Thrown"),
+    CAUGHT_FISH (ChatColor.YELLOW, "Fish Caught"),
+    MESSAGES_SENT (ChatColor.AQUA, "Sent Messages"),
+    CHARACTERS_TYPED (ChatColor.DARK_AQUA, "Sent Characters");
 
-    Tracked(ChatColor color) {}
+    public final ChatColor color;
+    public final String name;
+    Tracked(ChatColor color, String trackedName) {
+        this.color = color;
+        this.name = trackedName;
+    }
 }
